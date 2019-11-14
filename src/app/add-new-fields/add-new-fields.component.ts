d8be6d7f35c022fb2f4d8457394c40adac85b0e2
rpokosov <hasan83842@gmail.com>
2019-11-14 09:30:25 +0300

    add second form

--- src/app/add-new-fields/add-new-fields.component.ts
+++ src/app/add-new-fields/add-new-fields.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { VotesService } from '../votes.service';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add-new-fields',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNewFieldsComponent implements OnInit {
 
-  constructor() { }
+  faPlus = faPlus;
+
+  form = {
+    id: null,
+    link: '',
+    nameRus: '',
+    nameEng: '',
+    vote: null,
+  };
+
+  validMsg: string = '';
+
+  myForm : FormGroup;
+
+  constructor(public vS: VotesService) {
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(event) {
+    try {
+      if(!this.form.id || this.form.id === ''){
+        throw new Error("не заполнено id");
+      }
+
+      if(!this.form.nameRus && !this.form.nameEng){
+        throw new Error("не заполнено name");
+      }
+
+      this.vS.addNewItem(this.form)
+      this.form = {
+        id: null,
+        link: '',
+        nameRus: '',
+        nameEng: '',
+        vote: null,
+      };
+      this.validMsg = 'saved!'
+    }catch(error){
+      this.validMsg = 'error: ' + error.message
+    }
   }
 
 }
